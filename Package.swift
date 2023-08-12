12f4855ca1ce5a88c4f11aebba83f21b3b696901
V1taS <SosinVitalii@iCloud.com>
2023-08-12 16:38:19 +0200

    Fix version

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
       targets: ["FancySwiftUI"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/V1taS/FancyStyle.git", from: "1.3.0"),
+    .package(url: "https://github.com/V1taS/FancyStyle.git", from: "1.4.0"),
   ],
   targets: [
     .target(
